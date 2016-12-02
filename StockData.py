c69d5e3ff7bc159bd09fc2428c63a49ec868c542
Aidan Gray <aidancgray@gmail.com>
2016-12-02 18:55:04 -0500

    Current stock price scraper with user-inputted ticker name

---
+++ StockData.py
@@ -0,0 +1,17 @@
+from yahoo_finance import Share
+
+# Ask for user input of stock ticker
+# Prints out company name and current price
+def main():
+
+    ticker = str(raw_input("Stock Ticker: "))
+
+    company = Share(ticker)
+
+    company.refresh()
+
+    print company.get_name()
+    print "Current Price: ", company.get_price()
+
+
+main()
